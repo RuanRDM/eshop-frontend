701f7802357869ecf67bdbe8956f70f942258ed8
RuanRDM <ruan.delatorre.rd@gmail.com>
2023-10-14 16:33:38 -0300

    att

--- src/componentes/telas/Home.jsx
+++ src/componentes/telas/Home.jsx
@@ -1,7 +1,11 @@
+import React from 'react';
+import eshopLogo from '../../images/logo.png';
+
 const Home = () => (
     <div>
         <h1>eShop - LPE</h1>
+        <img src={eshopLogo} alt="eShop Logo" />
     </div>
-)
+);
 
 export default Home;
