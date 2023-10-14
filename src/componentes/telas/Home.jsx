19fc06263da87fc723d157fcbb34f32b39970891
RuanRDM <ruan.delatorre.rd@gmail.com>
2023-10-14 16:46:53 -0300

    att home

--- src/componentes/telas/Home.jsx
+++ src/componentes/telas/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import eshopLogo from '../../images/estudo-de-caso-lpe.png';
 
-const Home = () => (
-    <div>
+const Home = () => {
+    const containerStyle = {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh', // Isso define a altura para ocupar 100% da altura da viewport
+    };
+  
+    return (
+      <div style={containerStyle}>
         <h1>eShop - LPE</h1>
         <img src={eshopLogo} alt="eShop Logo" />
-    </div>
-);
+      </div>
+    );
+  };
 
 export default Home;
