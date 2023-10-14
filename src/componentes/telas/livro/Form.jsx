45884d7c9ca73f8163340b1f1f853b5fd7e9b14d
RuanRDM <ruan.delatorre.rd@gmail.com>
2023-10-14 16:23:15 -0300

    att form livros

--- src/componentes/telas/livro/Form.jsx
+++ src/componentes/telas/livro/Form.jsx
@@ -1,17 +1,17 @@
 import { useContext } from 'react'
 import Alerta from '../../comuns/Alerta';
-import ProdutoContext from './ProdutoContext';
+import LivroContext from './LivroContext';
 import CampoEntrada from '../../comuns/CampoEntrada';
 import Dialogo from '../../comuns/Dialogo';
 import CampoSelect from '../../comuns/CampoSelect';
 
 function Form() {
 
-    const { objeto, handleChange, acaoCadastrar, alerta, listaCategorias }
-        = useContext(ProdutoContext);
+    const { objeto, handleChange, acaoCadastrar, alerta, listaEditoras }
+        = useContext(LivroContext);
 
     return (
-        <Dialogo id="modalEdicao" titulo="Produto" idformulario="formEdicao"
+        <Dialogo id="modalEdicao" titulo="Livro" idformulario="formEdicao"
             acaoCadastrar={acaoCadastrar}>
             <Alerta alerta={alerta} />
             <CampoEntrada id="txtCodigo" label="Código" tipo="number"
@@ -19,50 +19,36 @@ function Form() {
                 handlechange={handleChange}
                 requerido={false} readonly={true}
                 maximocaracteres={5} />
-            <CampoEntrada id="txtNome" label="Nome" tipo="text"
-                name="nome" value={objeto.nome}
+            <CampoEntrada id="txtTitulo" label="Titulo" tipo="text"
+                name="titulo" value={objeto.titulo}
                 handlechange={handleChange}
                 requerido={true} readonly={false}
-                textovalido="Nome OK" textoinvalido="Informe o nome"
+                textovalido="Titulo OK" textoinvalido="Informe o Titulo"
                 maximocaracteres={40} />
+            <CampoEntrada id="txtAutor" label="Autor" tipo="number"
+                name="autor" value={objeto.autor}
+                handlechange={handleChange}
+                requerido={true} readonly={false}
+                textovalido="Autor OK" textoinvalido="Informe o Autor" />
+            <CampoEntrada id="txtPreco" label="Preço" tipo="number"
+                name="preco" value={objeto.preco}
+                handlechange={handleChange}
+                requerido={true} readonly={false}
+                textovalido="Preço OK" textoinvalido="Informe o Preço" />
             <CampoEntrada id="txtDescricao" label="Descrição" tipo="text"
                 name="descricao" value={objeto.descricao}
                 handlechange={handleChange}
                 requerido={true} readonly={false}
                 textovalido="Descrição OK" textoinvalido="Informe a descrição"
                 maximocaracteres={40} />
-            <CampoEntrada id="txtEstoque" label="Estoque" tipo="number"
-                name="quantidade_estoque" value={objeto.quantidade_estoque}
-                handlechange={handleChange}
-                requerido={true} readonly={false}
-                textovalido="Estoque OK" textoinvalido="Informe estoque" />
-            <CampoSelect id="txtAtivo" label="Ativo"
-                name="ativo" value={objeto.ativo}
+            <CampoSelect id="txtEditora" label="Editora"
+                name="editora" value={objeto.editora}
                 handlechange={handleChange}
                 requerido={true}
-                textovalido="Ativo OK"
-                textoinvalido="Informe se o produto está ativo">
-                <option value={true}>SIM</option>
-                <option value={false}>NÃO</option>
-            </CampoSelect>
-            <CampoEntrada id="txtValor" label="Valor" tipo="number"
-                name="valor" value={objeto.valor}
-                handlechange={handleChange}
-                requerido={true} readonly={false}
-                textovalido="Valor OK" textoinvalido="Informe o valor" />
-            <CampoEntrada id="txtDataCadastro" label="Data de cadastro" tipo="date"
-                name="data_cadastro" value={objeto.data_cadastro}
-                handlechange={handleChange}
-                requerido={true} readonly={false}
-                textovalido="Data OK" textoinvalido="Informe a data de cadastro" />
-            <CampoSelect id="txtCategoria" label="Categoria"
-                name="categoria" value={objeto.categoria}
-                handlechange={handleChange}
-                requerido={true}
-                textovalido="Categoria OK"
-                textoinvalido="Informe a categoria">
+                textovalido="Editora OK"
+                textoinvalido="Informe a Editora">
                 {
-                    listaCategorias.map((cat) => (
+                    listaEditoras.map((cat) => (
                         <option key={cat.codigo} value={cat.codigo}>
                             {cat.nome}
                         </option>
